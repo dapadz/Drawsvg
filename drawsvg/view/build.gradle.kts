32d6e144a57f2f9be2a609a329f358b31c391271
Igor Zakharov <i.zakharov@l-tech.ru>
2025-10-23 17:00:00 +0300

    add jitpack.yml

--- drawsvg/view/build.gradle.kts
+++ drawsvg/view/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
     namespace = "com.dapadz.drawsvg.view"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk { version = release(36) }
     defaultConfig {
         minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
@@ -28,10 +27,42 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(project(":drawsvg:core"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("Release") {
+                groupId = "com.dapadz"
+                artifactId = "drawsvg"
+                version = "1.0.0"
+                pom { name.set("drawsvg") }
+                from(components["release"])
+            }
+        }
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/dapadz/Drawsvg")
+                credentials {
+                    username = (findProperty("gpr.user") as String?)
+                        ?: System.getenv("USERNAME")
+                    password = (findProperty("gpr.key") as String?)
+                        ?: System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
 }
