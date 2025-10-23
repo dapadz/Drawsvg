a9703e2130a785a576bf22e24e0f9d1e314ee6c3
Igor Zakharov <i.zakharov@l-tech.ru>
2025-10-23 17:45:34 +0300

    add core publish plugin

--- drawsvg/core/build.gradle.kts
+++ drawsvg/core/build.gradle.kts
@@ -1,13 +1,32 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("maven-publish")
 }
+
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
 }
+
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "com.github.dapadz"
+            artifactId = "drawsvg-core"
+            version = "0.0.6"
+            from(components["java"])
+            pom {
+                name.set("drawsvg-core")
+                description.set("Core utilities for drawsvg")
+            }
+        }
     }
 }
